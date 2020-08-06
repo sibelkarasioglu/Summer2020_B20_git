61b752239198430cf06432b15455ea3b9d3898e7
sibelkarasioglu <68888878+sibelkarasioglu@users.noreply.github.com>
2020-08-05 21:32:49 -0400

    Update gitFun.java

--- src/utility/gitFun.java
+++ src/utility/gitFun.java
@@ -3,5 +3,8 @@ package utility;
 public class gitFun {
     public static void main(String[] args) {
         System.out.println("Everything is fun");
+        //this line is edit from Github
+        
+        
     }
 }
