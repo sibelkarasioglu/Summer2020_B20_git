3159e17f39f723da5fa7a241b2ef479760f0cec1
sibelkarasioglu <sibelkarasioglu@hotmail.com>
2020-08-05 20:29:13 -0400

    add new line

---
+++ src/utility/reverseASttring.java
@@ -0,0 +1,12 @@
+package utility;
+
+public class reverseASttring {
+    public static void main(String[] args) {
+        String str="school is fun";
+                String result = "";
+             for(int i = str.length()-1;  i >=0 ; i--){
+                 result += str.charAt(i);
+             }
+        System.out.println(result);
+    }
+}
